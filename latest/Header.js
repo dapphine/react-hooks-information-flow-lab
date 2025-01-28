2e2a466dcfe142b3db21069132cbb2bae53ee8c5
dapphine <dapphine.michere@student.moringaschool.com>
2025-01-28 16:27:25 +0300

    final commit

---
+++ latest/Header.js
@@ -0,0 +1,13 @@
+import React from"react";
+import 'src/index.css';
+
+const Header =()=>{
+    return(
+        <header>className ="header"
+            <div className="Logo -container">
+
+            </div>
+
+        </header>
+    )
+}
---
+++ latest/src/components/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+import Header from "./Header";
+import Filter from "./Filter";
+import Item from "./Item";
+import itemData from "../data/items";
+
+function App() {
+  const [items, setItems] = useState(itemData);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function onDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
+  return (
+    <div className={"App " + (isDarkMode ? "dark" : "light")}>
+      <Header isDarkMode={isDarkMode} onDarkModeClick={onDarkModeClick} />
+      <ShoppingList items={items} />
+    </div>
+  );
+}
+
+export default App;
